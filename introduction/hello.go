d4061b5647c2c8e47c6099aea2a82b6fd973d3bc
Tobetch <takutobe@gmail.com>
2020-08-02 22:42:47 +0900

    add HelloWorld

---
+++ introduction/hello.go
@@ -0,0 +1,17 @@
+// 標準ライブラリ一覧
+// math：算術用。Sin, Cos, Tan, Sqrtなど
+// compress：圧縮用
+// crypto：圧縮用
+// image：画像処理用
+package main
+
+// フォーマット用関数 "fmt" をインポート
+import (
+	"fmt"
+)
+
+// main関数の宣言
+// Goプログラムはmainパッケージのmain関数から実行される
+func main() {
+	fmt.Println("Hello, world") //Javaにおける"System.out.println"メソッド
+}
