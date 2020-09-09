f7fc63d07a908cf4cdf46775b0efc286e7890a71
Tobetch <takutobe@gmail.com>
2020-09-10 08:35:27 +0900

    add tmp file

---
+++ introduction/Lesson2/mars.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	mars()
+	tryFmt()
+}
+
+func mars() {
+	fmt.Print("火星の表面では、とべの体重は")
+	fmt.Print(64 * 0.3783)
+	fmt.Print("Kgに、年齢は")
+	fmt.Print(30 * 365 / 687)
+	fmt.Print("歳になるでしょう。")
+}
+
+func tryFmt() {
+
+}
