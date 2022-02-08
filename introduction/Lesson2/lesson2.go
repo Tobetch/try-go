2b1c6a70685793669bdf19c9a61891ce0a39cb4d
Tobetch <takutobe@gmail.com>
2022-02-09 08:55:58 +0900

    add shortcut

--- introduction/Lesson2/lesson2.go
+++ introduction/Lesson2/lesson2.go
@@ -25,3 +25,15 @@ func lightspeed() {
 	distance = 401000000
 	fmt.Println(distance/lightSpeed, "秒")
 }
+
+func shortcut() {
+	var weight = 149.0
+	weight = weight * 0.3783
+	weight *= 0.3783
+
+	var age = 41
+	age = age + 1
+	age += 1
+	age++
+	// ++ageはサポートしない
+}
