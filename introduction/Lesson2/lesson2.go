9d060ee70bc5adacc44058448696b64b34d53141
Tobetch <takutobe@gmail.com>
2022-02-09 08:49:32 +0900

    add lightspeed

--- introduction/Lesson2/lesson2.go
+++ introduction/Lesson2/lesson2.go
@@ -6,7 +6,7 @@ import (
 
 func main() {
 	mars()
-	tryPrintf()
+	lightspeed()
 }
 
 func mars() {
@@ -14,8 +14,14 @@ func mars() {
 	const age = 30 * 365 / 687
 	fmt.Print("火星の表面では、とべの体重は", weight, "Kgに、")
 	fmt.Println("年齢は", age, "歳になるでしょう。")
+	fmt.Printf("火星の表面では、とべの体重はだいたい%dKgになります。\n", 24)
 }
 
-func tryPrintf() {
-	fmt.Printf("火星の表面では、とべの体重はだいたい%dKgになります。", 24)
+func lightspeed() {
+	const lightSpeed = 299792 // km
+	var distance = 56000000   // km
+	fmt.Println(distance/lightSpeed, "秒")
+
+	distance = 401000000
+	fmt.Println(distance/lightSpeed, "秒")
 }
