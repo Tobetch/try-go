15a05ee9c1408e5249e0f87d4b38187d42678b44
Tobetch <takutobe@gmail.com>
2023-06-08 19:24:06 +0900

    Call your code from another module

--- official_tutorial/hello/hello.go
+++ official_tutorial/hello/hello.go
@@ -3,11 +3,15 @@ package main
 import (
 	"fmt"
 
+	"example.com/greetings"
+
 	"rsc.io/quote"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Get a greeting message and print it.
+	message := greetings.Hello("Gladys")
+	fmt.Println(message)
 
 	// Call code in an external package
 	fmt.Println(quote.Go())
