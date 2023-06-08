f3db19d4d577bfbaf0210f4b2b5c307e20037a80
Tobetch <takutobe@gmail.com>
2023-06-08 17:29:16 +0900

    Call code in an external package

--- official_tutorial/hello/hello.go
+++ official_tutorial/hello/hello.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	// Call code in an external package
+	fmt.Println(quote.Go())
 }
