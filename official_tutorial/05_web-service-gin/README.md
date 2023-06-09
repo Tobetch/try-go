c23d4e0c1fa04cffae2d0629a2ef66ff4eed368d
Tobetch <takutobe@gmail.com>
2023-06-09 17:46:24 +0900

    Write a handler to add a new item

--- official_tutorial/05_web-service-gin/README.md
+++ official_tutorial/05_web-service-gin/README.md
@@ -1,3 +1,11 @@
 # URL
 
 - [Tutorial: Developing a RESTful API with Go and Gin](https://go.dev/doc/tutorial/web-service-gin)
+
+## CURL
+
+- GET
+  - ex: ```curl http://localhost:8080/albums```
+- POST
+  - ex:
+  ```curl http://localhost:8080/albums --include --header "Content-Type: application/json" --request "POST" --data '{"id": "4","title": "The Modern Sound of Betty Carter","artist": "Betty Carter","price": 49.99}'```
--- official_tutorial/05_web-service-gin/main.go
+++ official_tutorial/05_web-service-gin/main.go
@@ -21,15 +21,31 @@ var albums = []album{
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbums)
+
+	// see curl.log for request(curl http://localhost:8080/albums)
+	router.Run("localhost:8080")
+}
+
 // getAlbums responds with the list of all albums as JSON.
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
-func main() {
-	router := gin.Default()
-	router.GET("/albums", getAlbums)
+// postAlbums adds an album from JSON received in the request body.
+func postAlbums(c *gin.Context) {
+	var newAlbum album
 
-	// see curl.log for request(curl http://localhost:8080/albums)
-	router.Run("localhost:8080")
+	// Call BindJSON to bind the received JSON to
+	// newAlbum.
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	// Add the new album to the slice.
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
