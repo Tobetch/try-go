495e433db9ff31a399ee4dc9c81e50bb9319b212
Tobetch <takutobe@gmail.com>
2023-06-12 16:14:51 +0900

    remove unused functions

--- official_tutorial/07_fuzz/reverse_test.go
+++ official_tutorial/07_fuzz/reverse_test.go
@@ -5,22 +5,6 @@ import (
 	"unicode/utf8"
 )
 
-func TestReverse(t *testing.T) {
-	testcases := []struct {
-		in, want string
-	}{
-		{"Hello, world", "dlrow ,olleH"},
-		{" ", " "},
-		{"!12345", "54321!"},
-	}
-	for _, tc := range testcases {
-		rev := Reverse(tc.in)
-		if rev != tc.want {
-			t.Errorf("Reverse: %q, want %q", rev, tc.want)
-		}
-	}
-}
-
 func FuzzReverse(f *testing.F) {
 	testcases := []string{"Hello, world", " ", "!12345"}
 	for _, tc := range testcases {
@@ -43,3 +27,40 @@ func FuzzReverse(f *testing.F) {
 		}
 	})
 }
+
+// func TestReverse(t *testing.T) {
+// 	testcases := []struct {
+// 		in, want string
+// 	}{
+// 		{"Hello, world", "dlrow ,olleH"},
+// 		{" ", " "},
+// 		{"!12345", "54321!"},
+// 	}
+// 	for _, tc := range testcases {
+// 		rev := Reverse(tc.in)
+// 		if rev != tc.want {
+// 			t.Errorf("Reverse: %q, want %q", rev, tc.want)
+// 		}
+// 	}
+// }
+
+// func FuzzReverseWithError(f *testing.F) {
+// 	testcases := []string{"Hello, world", " ", "!12345"}
+// 	for _, tc := range testcases {
+// 		f.Add(tc) // Use f.Add to provide a seed corpus
+// 	}
+// 	f.Fuzz(func(t *testing.T, orig string) {
+// 		rev := ReverseWithInvalidStringError(orig)
+// 		// rev := ReversWithDoubleReverseError(orig)
+// 		doubleRev := ReverseWithInvalidStringError(rev)
+// 		// doubleRev := ReversWithDoubleReverseError(rev)
+// 		// Diagnose the error
+// 		t.Logf("Number of runes: orig=%d, rev=%d, doubleRev=%d", utf8.RuneCountInString(orig), utf8.RuneCountInString(rev), utf8.RuneCountInString(doubleRev))
+// 		if orig != doubleRev {
+// 			t.Errorf("Before: %q, after: %q", orig, doubleRev)
+// 		}
+// 		if utf8.ValidString(orig) && !utf8.ValidString(rev) {
+// 			t.Errorf("Reverse produced invalid UTF-8 string %q", rev)
+// 		}
+// 	})
+// }
