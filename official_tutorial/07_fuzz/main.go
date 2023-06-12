0733de999633a4beaac588665e754bc92bcd600b
Tobetch <takutobe@gmail.com>
2023-06-12 16:09:57 +0900

    rename functions

--- official_tutorial/07_fuzz/main.go
+++ official_tutorial/07_fuzz/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 // using byte for reverse
-func ReverseWithError(s string) string {
+func ReverseWithInvalidStringError(s string) string {
 	b := []byte(s)
 	for i, j := 0, len(b)-1; i < len(b)/2; i, j = i+1, j-1 {
 		b[i], b[j] = b[j], b[i]
@@ -12,6 +12,14 @@ func ReverseWithError(s string) string {
 }
 
 // using rune for reverse
+func ReversWithDoubleReverseError(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
+
 func Reverse(s string) string {
 	r := []rune(s)
 	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
