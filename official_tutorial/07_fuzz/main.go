343e2ee4f8c3acefca31bb4fb49bdf9daabb04b4
Tobetch <takutobe@gmail.com>
2023-06-12 15:15:51 +0900

    Fix the invalid string error

--- official_tutorial/07_fuzz/main.go
+++ official_tutorial/07_fuzz/main.go
@@ -2,12 +2,20 @@ package main
 
 import "fmt"
 
-func Reverse(s string) string {
+func ReverseWithError(s string) string {
 	b := []byte(s)
 	for i, j := 0, len(b)-1; i < len(b)/2; i, j = i+1, j-1 {
 		b[i], b[j] = b[j], b[i]
 	}
 	return string(b)
+}
+
+func Reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
 }
 
 func main() {
